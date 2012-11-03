35020ca668a21d8ccad735e6b827c26463ba09f5
libra guest <davendw@gmail.com>
2012-11-03 14:11:33 -0400

    Rails with thin

---
+++ coursefeedback/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coursefeedback::Application.config.secret_token = 'd268655c6ff4dd89f6dc018e27530fc2c90ff9f82bdb90368483519f9ab66c20f378fe5ebed5c76689e1eb9e048041505f9a0a31866ae0680eafd9259cff265e'
